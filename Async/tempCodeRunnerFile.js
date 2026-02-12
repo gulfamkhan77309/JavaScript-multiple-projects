8efb11c7eb9106b948b416d124d9977ecfbfc75e
Gulfam khan <gulfamkhan77309@gmail.com>
2026-02-13 03:25:35 +0530

    Delete Async/tempCodeRunnerFile.js

--- Async/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-consumePromiseFive()
-
-// async function getUserData () {
-//     const response = await fetch("https://randomuser.me/api/")
-//     const data = await response.json()
-//     console.log(data);
-    
-// }
-// getUserData()
